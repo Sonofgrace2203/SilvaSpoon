f0374178d204c21f75f0632bbadbc1cba4a6159a
Sonofgrace2203 <Sonofgrace2203@users.noreply.github.com>
2026-08-15 07:36:29 +0000

    deploy: 4e797689616dbbcd4c9a1905a0d1807b58c98c39

---
+++ js/app-refresh.js
@@ -0,0 +1,15 @@
+window.silvaSpoonRefreshDotNet = null;
+
+window.silvaSpoonSetRefreshReference = function (dotNetReference) {
+    window.silvaSpoonRefreshDotNet = dotNetReference;
+};
+
+window.silvaSpoonRefresh = function () {
+    if (window.silvaSpoonRefreshDotNet) {
+        return window.silvaSpoonRefreshDotNet.invokeMethodAsync(
+            "RefreshCurrentPage"
+        );
+    }
+
+    return Promise.resolve();
+};
